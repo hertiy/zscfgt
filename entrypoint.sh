f3ce7c24f3990ccd068ae862b1e68fc626d64f82
hertiy <154105959+hertiy@users.noreply.github.com>
2023-12-18 13:34:14 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,22 +7,22 @@ fi
 echo ${VER}
 
 if [[ -z "${Vless_Path}" ]]; then
-  Vless_Path="/394fe03d-289a-4dea-b9b5-0bed468260f3"
+  Vless_Path="/c8a13923-789c-45f8-a3f1-ecb7b2a31521"
 fi
 echo ${Vless_Path}
 
 if [[ -z "${Vless_UUID}" ]]; then
-  Vless_UUID="394fe03d-289a-4dea-b9b5-0bed468260f3"
+  Vless_UUID="c8a13923-789c-45f8-a3f1-ecb7b2a31521"
 fi
 echo ${Vless_UUID}
 
 if [[ -z "${Vmess_Path}" ]]; then
-  Vmess_Path="/5d293e61-305e-4003-ac87-4587b36646aa"
+  Vmess_Path="/7dc83690-c01e-4531-a775-08b3699d64a5"
 fi
 echo ${Vmess_Path}
 
 if [[ -z "${Vmess_UUID}" ]]; then
-  Vmess_UUID="5d293e61-305e-4003-ac87-4587b36646aa"
+  Vmess_UUID="7dc83690-c01e-4531-a775-08b3699d64a5"
 fi
 echo ${Vmess_UUID}
 
